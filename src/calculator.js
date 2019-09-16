b1565fdf24d5fd44ba6d601696a75f2b38d063c5
Ferronique <ferronique.may@umuzi.org>
2019-09-16 09:51:15 +0200

    feedback suggested changes made.

--- src/calculator.js
+++ src/calculator.js
@@ -1,4 +1,3 @@
-//add function
 function add(){
     let sum=0;
     for (let i = 0; i < arguments.length; i++) {
@@ -7,7 +6,6 @@ function add(){
     return sum;
 }
 
-//multiply function
 function multiply(){
     let product=1;
     for (let i = 0; i < arguments.length; i++) {
